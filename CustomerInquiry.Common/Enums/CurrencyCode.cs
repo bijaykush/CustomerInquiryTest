adac460f3754e62f0e7b7841a4449ad9b77adb8e
bijaykush <bijaykush2012@gmail.com>
2019-04-14 11:50:43 +0545

    added enums for currency code and transaction status

---
+++ CustomerInquiry.Common/Enums/CurrencyCode.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomerInquiry.Common.Enums
+{
+    public enum CurrencyCode
+    {
+        USD = 1,
+        JPY = 2,
+        THB = 3,
+        SGD = 4
+    }
+}
---
+++ CustomerInquiry.Common/Enums/TransactionStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerInquiry.Common.Enums
+{
+    public enum TransactionStatus
+    {
+        Success = 1,
+        Failed = 2,
+        Cancelled = 3
+    }
+}
