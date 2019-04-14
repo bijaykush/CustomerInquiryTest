75c949da387693e784ab4261df10e2e8df82d5aa
bijaykush <bijaykush2012@gmail.com>
2019-04-14 12:39:07 +0545

    Added repo for customer with business logic to get 5 latest transactions of a customers

---
+++ CustomerInquiry.Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerInquiry.Data.DataContext;
+using CustomerInquiry.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerInquiry.Data.Repositories
+{
+    public class CustomerRepository : ICustomerRepository, IDisposable
+    {
+        private readonly CustomerInquiryDBContext _customerInquiryDBContext;
+        public CustomerRepository(CustomerInquiryDBContext customerInquiryDBContext)
+        {
+            _customerInquiryDBContext = customerInquiryDBContext;
+        }
+
+
+        public async Task<CustomerProfile> GetCustomersAsync(CustomerInquiryFilter customerInquiryFilter)
+        {
+            var query = (from c in _customerInquiryDBContext.Customers
+                         join t in _customerInquiryDBContext.Transactions on c.CustomerId equals t.CustomerId
+                         select new CustomerProfile
+                         {
+                             CustomerId = c.CustomerId,
+                             Email = c.ContactEmail,
+                             Name = c.CustomerName,
+                             MobileNumber = c.MobileNumber,
+                             RecentTransactions = c.RecentTransactions.OrderByDescending(x => x.TransactionDateTime).Take(5).ToList()
+                         });
+
+            if (string.IsNullOrEmpty(customerInquiryFilter.Email))
+            {
+                query = query.Where(x => x.CustomerId == customerInquiryFilter.CustomerId);
+            }
+            else if (customerInquiryFilter.CustomerId == null)
+            {
+                query = query.Where(x => x.Email == customerInquiryFilter.Email);
+            }
+            else
+            {
+                query = query.Where(x => x.CustomerId == customerInquiryFilter.CustomerId && x.Email == customerInquiryFilter.Email);
+            }
+
+            return await query.FirstOrDefaultAsync();
+
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _customerInquiryDBContext.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        
+    }
+}
---
+++ CustomerInquiry.Data/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using CustomerInquiry.Data.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace CustomerInquiry.Data.Repositories
+{
+    public interface ICustomerRepository: IDisposable
+    {
+        Task<CustomerProfile> GetCustomersAsync(CustomerInquiryFilter customerInquiry);
+    }
+}
