250f9dc7ed1e14c66036db89480a0d1139205bba
bijaykush <bijaykush2012@gmail.com>
2019-04-14 11:58:28 +0545

    Added ICustomerInquiryService interface

---
+++ CustomerInquiry.Service/Interfaces/ICustomerInquiryService.cs
@@ -0,0 +1,10 @@
+﻿using CustomerInquiry.Service.DTO;
+using System.Threading.Tasks;
+
+namespace CustomerInquiry.Service.Interfaces
+{
+    public interface ICustomerInquiryService
+    {
+        Task<CustomerProfileDTO> GetCustomerProfile(CustomerInquiryFilterDTO customerInquiry);
+    }
+}
